acce8f2e27c37e940c0687aa494629d096d50b45
Ga30r <54589984+Ga30r@users.noreply.github.com>
2022-04-12 16:25:07 +0100

    IPFS data update

--- JSON_file_creat/go.mod
+++ JSON_file_creat/go.mod
@@ -2,4 +2,4 @@ module main.go
 
 go 1.16
 
-require github.com/ipfs/go-ipfs-api v0.3.0 // indirect
+require github.com/ipfs/go-ipfs-api v0.3.0
--- JSON_file_creat/main.go
+++ JSON_file_creat/main.go
@@ -11,12 +11,13 @@ import (
 
 // TimeSeriesDatum is the structure used to store a single time series data
 type TimeSeriesDatum struct {
-	Id_number    uint64 `json:"id"`
+	Id_number    string `json:"NINO"`
 	Name string  `json:"name"`
+	Address string  `json:"address"`
 	
 }	
 
-func main() {
+func IPFS_add(name_cal string , address_cal string ,id_num_cal string) {
 	//
 	// Connect to your local IPFS deamon running in the background.
 	//
@@ -29,10 +30,13 @@ func main() {
 	//
 
 	tsd := &TimeSeriesDatum{
-		Id_number: 2123198,
-		Name: "Ben Brown",
+		Id_number: id_num_cal,
+		Address: address_cal,
+		Name: name_cal,
+		
 		
 	}
+	
 	tsdBin, _ := json.Marshal(tsd)
 	reader := bytes.NewReader(tsdBin)
 
@@ -65,3 +69,20 @@ func main() {
 	fmt.Println(res)
 }
 
+func main(){
+
+	//data from https://www.fakenamegenerator.com/gen-random-en-uk.php
+	//moved from address A to adddress B
+	IPFS_add("Paige Buckley","22 Grenoble Road BREAKISH IV42 4DH","XA 50 30 34 B")
+	IPFS_add("Paige Buckley","14 Grenoble Road BREAKISH IV42 4DH","XA 50 30 34 B")
+	
+	
+	IPFS_add("Connor Elliott","6 Great North Road ALWOODLEY PARK LS17 9ND","YR 79 78 34 B")
+	IPFS_add("Noah Morrison","7 Dover Road WESTHAM BN24 7WW","AR 59 07 88 ")
+	IPFS_add("Lola Kirby","9 Hart Road NORTHBOURNE CT14 5LS ","EN 94 73 70 D")
+	IPFS_add("Mathias Aachen","Gotzkowskystraße 14 46348 Raesfeld","NA")
+	IPFS_add("Mujahid Juwain Najjar","Coquimbo 9385 65002 Nuevo Berlín","NA")
+	
+	
+	
+}
