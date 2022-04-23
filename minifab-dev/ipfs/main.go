08dd68e84fd562e59f69008a07eb80245cf1baef
Ga30r <54589984+Ga30r@users.noreply.github.com>
2022-04-23 19:21:04 +0100

    Update main.go

--- minifab-dev/ipfs/main.go
+++ minifab-dev/ipfs/main.go
@@ -6,10 +6,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-
 	"github.com/google/uuid"
 	shell "github.com/ipfs/go-ipfs-api"
+	//crypto
+	 "crypto/aes"
+	 "crypto/cipher"
+ 	"encoding/base64"
+ 	
 )
+
+
 
 var myslice []string
 
@@ -19,6 +25,62 @@ type TimeSeriesDatum struct {
 	Name      string `json:"name"`
 	Address   string `json:"address"`
 }
+
+
+
+
+//cypto stuff
+
+
+
+
+var cbytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
+// This should be in an env file in production
+const MySecret string = "abc&1*~#^2^#s0^=)^^7%b34"
+func Encode(b []byte) string {
+ return base64.StdEncoding.EncodeToString(b)
+}
+
+
+//decrypt
+func Decode(s string) []byte {
+ data, err := base64.StdEncoding.DecodeString(s)
+ if err != nil {
+  panic(err)
+ }
+ return data
+} 
+
+
+// Decrypt method is to extract back the encrypted text
+func Decrypt(text, MySecret string) (string, error) {
+ block, err := aes.NewCipher([]byte(MySecret))
+ if err != nil {
+  return "", err
+ }
+ cipherText := Decode(text)
+ cfb := cipher.NewCFBDecrypter(block, cbytes)
+ plainText := make([]byte, len(cipherText))
+ cfb.XORKeyStream(plainText, cipherText)
+ return string(plainText), nil
+}
+
+
+
+// Encrypt method is to encrypt or hide any classified text
+func Encrypt(text, MySecret string) (string, error) {
+ block, err := aes.NewCipher([]byte(MySecret))
+ if err != nil {
+  return "", err
+ }
+ plainText := []byte(text)
+ cfb := cipher.NewCFBEncrypter(block, cbytes)
+ cipherText := make([]byte, len(plainText))
+ cfb.XORKeyStream(cipherText, plainText)
+ return Encode(cipherText), nil
+}
+
+
 
 func IPFS_add(name_cal string, address_cal string, id_num_cal string) {
 	//
@@ -77,6 +139,7 @@ func IPFS_add(name_cal string, address_cal string, id_num_cal string) {
 
 func main() {
 
+    
 	arg := os.Args[1]
 
 	if arg == "1" {
@@ -87,13 +150,42 @@ func main() {
 		fmt.Println("2. Please Enter Address: ")
 		var address string
 		fmt.Scanln(&address)
-
 		fmt.Println("3. Please Enter Nino: ")
 		var nino string
 		fmt.Scanln(&nino)
-		IPFS_add(name, address, nino)
-	} else {
+		//name encrypt
+		encname, err := (Encrypt(name, MySecret))
+			if err != nil {
+     				fmt.Println("error encrypting your classified text: ", err)
+    				} 				   				
+    				
+    		//address encrypt
+		encaddress, err := (Encrypt(address, MySecret))
+			if err != nil {
+     				fmt.Println("error encrypting your classified text: ", err)
+    				}
+    		//Nino encrypt
+		encNino, err := (Encrypt(nino, MySecret))
+			if err != nil {
+     				fmt.Println("error encrypting your classified text: ", err)
+    				}				
+    		IPFS_add(encname, encaddress, encNino)
+    		//Decrypt name
+		decname, err := Decrypt(encname, MySecret)
+ 		fmt.Println("Encrypted name:",encname ,"		Decrypt name:",decname)
+ 		
+ 		//Decrypt address
+ 		decaddress, err := Decrypt(encaddress, MySecret)
+ 		fmt.Println("Encrypted address:",encaddress ,"		Decrypt address:",decaddress)
+ 		
+ 		//Decrypt Nino
+ 		decaNino, err := Decrypt(encNino, MySecret)
+ 		fmt.Println("Encrypted Nino:",encaddress ,"		Decrypt Nino:",decaNino)
 
+
+		
+	} else {
+		
 		//data from https://www.fakenamegenerator.com/gen-random-en-uk.php
 		//moved from address A to adddress B
 		IPFS_add("Lola Kirby", "9 Hart Road NORTHBOURNE CT14 5LS ", "EN 94 73 70 D")
@@ -104,3 +196,5 @@ func main() {
 	}
 
 }
+
+
